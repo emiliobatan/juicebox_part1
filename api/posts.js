8c040fcd575a4fce325db43dc582c9479c79b0c0
Emilio <emiliobatan@ymail.com>
2021-09-19 18:25:24 -0700

    All done

--- api/posts.js
+++ api/posts.js
@@ -1,10 +1,33 @@
+const { response } = require('express');
 const express = require('express');
 const postsRouter = express.Router();
-const { getAllPosts } = require('../db');
+const { getAllPosts, createPost, updatePost, getPostById } = require('../db');
 const { requireUser } = require('./utils');
 
 postsRouter.post('/', requireUser, async (req, res, next) => {
-    res.send({ message: 'under construction' });
+    const { title, content, tags = "" } = req.body;
+
+    const tagArr = tags.trim().split(/\s+/)
+    const postData = { title, content, tags }; 
+
+    if (tagArr.length) { 
+        postData.tags = tagArr;
+    }
+
+    try { 
+        const post = await createPost(postData);
+
+        if (post) { 
+            res.send({ post });
+        } else { 
+            next({
+                name: 'Posting Error',
+                message: 'Error trying to post'
+            })
+        }
+    } catch ({ name, message }) { 
+        next({ name, message })
+    }
   });
 
   
@@ -14,14 +37,90 @@ postsRouter.use((req, res, next) => {
     next();
 });
 
+postsRouter.patch('/:postId', requireUser, async (req, res, next) => {
+    const { postId } = req.params;
+    const { title, content, tags } = req.body;
+  
+    const updateFields = {};
+  
+    if (tags && tags.length > 0) {
+      updateFields.tags = tags.trim().split(/\s+/);
+    }
+  
+    if (title) {
+      updateFields.title = title;
+    }
+  
+    if (content) {
+      updateFields.content = content;
+    }
+  
+    try {
+      const originalPost = await getPostById(postId);
+  
+      if (originalPost.author.id === req.user.id) {
+        const updatedPost = await updatePost(postId, updateFields);
+        res.send({ post: updatedPost })
+      } else {
+        next({
+          name: 'UnauthorizedUserError',
+          message: 'You cannot update a post that is not yours'
+        })
+      }
+    } catch ({ name, message }) {
+      next({ name, message });
+    }
+  });
+
+  
+  postsRouter.delete('/:postId', requireUser, async (req, res, next) => {
+    try {
+      const post = await getPostById(req.params.postId);
+  
+      if (post && post.author.id === req.user.id) {
+        const updatedPost = await updatePost(post.id, { active: false });
+  
+        res.send({ post: updatedPost });
+      } else {
+        // if there was a post, throw UnauthorizedUserError, otherwise throw PostNotFoundError
+        next(post ? { 
+          name: "UnauthorizedUserError",
+          message: "You cannot delete a post which is not yours"
+        } : {
+          name: "PostNotFoundError",
+          message: "That post does not exist"
+        });
+      }
+  
+    } catch ({ name, message }) {
+      next({ name, message })
+    }
+  });
+
 
 
 postsRouter.get('/', async (req, res) => { 
-    const posts = await getAllPosts();
+    try { 
+        const allPosts = await getAllPosts(); 
 
-    res.send({
-        posts
-    })
+        const posts = allPosts.filter(post => { 
+            if (post.active) { 
+                return true; 
+            }
+
+            if (req.user && post.author.id === req.user.id) { 
+                return true;
+            }
+
+            return false;
+        });
+
+        res.send({
+            posts
+        });
+    } catch ({ name, message }) { 
+        next({ name, message });
+    }
 });
 
 module.exports = postsRouter;
--- api/tags.js
+++ api/tags.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const tagsRouter = express.Router();
-const { getAllTags } = require('../db');
+const { getAllTags, getPostsByTagName } = require('../db');
 
 tagsRouter.use((req, res, next) => { 
     console.log("A request is being made to /tags");
@@ -9,12 +9,19 @@ tagsRouter.use((req, res, next) => {
     console.log('This is next: ', next);
 });
 
-tagsRouter.get('/', async (req, res) => {
-    const tags = await getAllTags();
+tagsRouter.get('/:tagName/posts', async (req, res, next) => {
+    // read the tagname from the params
+    const tag = req.params.tagName
+    try {
+      // use our method to get posts by tag name from the db
+      // send out an object to the client { posts: // the posts }
+      const posts = await getPostsByTagName(tag)
 
-    res.send({
-        tags
-    })
-});
+      res.send({posts:posts})
+    } catch ({ name, message }) {
+        next({ name, message });
+        // forward the name and message to the error handler
+    }
+  });
 
 module.exports = tagsRouter;
